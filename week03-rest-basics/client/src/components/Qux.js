6070604386915ee843aa3f78ddbd7357e6f18272
a.student on Pristine Lubuntu <a.student@example.com>
2019-04-23 17:22:58 -0700

    proptypes

--- week03-rest-basics/client/src/components/Qux.js
+++ week03-rest-basics/client/src/components/Qux.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export const Qux = ({queryServer}) => (
     <button data-url="/qux-you-rang" onClick={queryServer}>Ring Qux</button>
 );
+
+Qux.propTypes =  {
+	queryServer: PropTypes.func
+};
