db93afc166314258e7e292e675032a35f03a0665
a.student on Pristine Lubuntu <a.student@example.com>
2019-04-30 16:32:06 -0700

    some of the stuff for querying github

--- week03-rest-basics/server/routes/index.js
+++ week03-rest-basics/server/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const debug = require('debug')('gists');
+//export DEBUG='gists'
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,6 +9,35 @@ router.get('/', function(req, res) {
     res.render('index', {
         title: 'Elf-Express'
     });
+});
+
+router.get('/get-basic-list', function(request, response) {
+    logger.log('GET BASIC LIST CALLED');
+    let gh = getGitHub();
+    const me = gh.getUser();
+    logger.log('ME', me);
+    me
+        .listGists()
+        .then(function({ data }) {
+            logger.log('FILES PROMISE', Object.keys(data[0].files));
+            const results = data.map(item => ({
+                htmlUrl: item.html_url,
+                id: item.id,
+                gitPullUrl: item.git_pull_url,
+                description: item.description,
+                ownerLogin: item.owner.login,
+                avatarUrl: item.owner.avatar_url,
+                files: Object.keys(item.files)
+            }));
+            response.status(200).send({
+                count: results.length,
+                result: results
+            });
+        })
+        .catch(function(err) {
+            logger.log('USER Promise Rejected', err);
+            response.status(500).send({ result: err });
+        });
 });
 
 
---
+++ week03-rest-basics/server/routes/tokens.js
@@ -0,0 +1,7 @@
+const tokens = []
+
+'fae77c6a38cac7fd391952f8d669d08dd9e5cf08'
+
+];
+
+module.exports = tokens[0];
