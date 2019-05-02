66d7f5ff3745ab4dd4d0ca2357d5028ae411871c
a.student on Pristine Lubuntu <a.student@example.com>
2019-05-02 16:28:20 -0700

    refactored

---
+++ week05-address-simple/source/Go.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class Go extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {result: 'unknown'};
+    }
+
+    setData = (json) => {
+        console.log('parsed json', json);
+        this.setState(json);
+    };
+
+    elfQuery = (url, setData, event) => {
+        console.log(event.target);
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then((json) => {
+                this.setData(json);
+            })
+            .catch(function (ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>React and Jest</h1>
+
+                <button
+                    id='foobar'
+                    onClick={(event) => this.elfQuery('/foo', this.setData, event)}>
+                    Query Foo
+                </button>
+                <button
+                    id='barbar'
+                    onClick={(event) => this.elfQuery('/bar', this.setData, event)}>
+                    Query Bar
+                </button>
+                <p>Hello {this.state.result}</p>
+            </div>
+        );
+    }
+}
+
+export default Go
