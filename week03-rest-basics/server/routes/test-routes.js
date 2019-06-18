8b78d456c4c5f2d7cac99b019d6fe6f5689077fb
a.student on Pristine Lubuntu <a.student@example.com>
2019-06-17 22:21:05 -0700

    this is eslint and prettier

--- week03-rest-basics/server/routes/test-routes.js
+++ week03-rest-basics/server/routes/test-routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Set up a route called foo. */
 router.get('/foo', function(request, response) {
-    var message = { 'result': 'success', 'status': 'bar', 'file': 'test-routes.js' };
+    var message = { result: 'success', status: 'bar', file: 'test-routes.js' };
     console.log('Foo called:\n' + JSON.stringify(message, null, 4));
     response.send(message);
 });
