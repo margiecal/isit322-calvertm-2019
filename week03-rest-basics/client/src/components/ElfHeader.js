75a6b4cd586c71c4c7b6f46d380bd88c7555f58b
a.student on Pristine Lubuntu <a.student@example.com>
2019-06-21 16:01:59 -0700

    got the header working

---
+++ week03-rest-basics/client/src/components/ElfHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../tree.svg';
+
+const ElfHeader = () => (
+     <div className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h2>Welcome to React</h2>
+     </div>
+);
+
+
+export default ElfHeader;
